df73486eb0570f4f98a053f5273b65ea4d08690f
Gonçalo Passos <mail@diogopassos.pt>
2024-02-26 12:45:18 +0000

    test: add ipv4 private address family tests

--- src/ip/ip.test.ts
+++ src/ip/ip.test.ts
@@ -10,4 +10,29 @@ describe("IPAddress", () => {
     const ip = IPAddress.parse("::1")
     expect(ip.version).toBe("v6")
   })
+
+  it("should return the ip address in string format", () => {
+    const ip = IPAddress.parse("127.0.0.1")
+    expect(ip.toString()).toBe("127.0.0.1")
+  })
+
+  it("ipv4 private address families", () => {
+    let ip = IPAddress.parse("192.168.0.1")
+    expect(ip.getIPAddressType()).toBe("Private")
+
+    ip = IPAddress.parse("192.168.1.10")
+    expect(ip.getIPAddressType()).toBe("Private")
+
+    ip = IPAddress.parse("192.168.255.255")
+    expect(ip.getIPAddressType()).toBe("Private")
+
+    ip = IPAddress.parse("10.0.0.0")
+    expect(ip.getIPAddressType()).toBe("Private")
+
+    ip = IPAddress.parse("10.10.10.10")
+    expect(ip.getIPAddressType()).toBe("Private")
+
+    ip = IPAddress.parse("10.255.255.255")
+    expect(ip.getIPAddressType()).toBe("Private")
+  })
 })
