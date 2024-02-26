a53dfd2876e9ff85f33158c723dab863d5589417
Gonçalo Passos <mail@diogopassos.pt>
2024-02-26 16:20:05 +0000

    test: update cidr test

--- src/cidr/cidr.test.ts
+++ src/cidr/cidr.test.ts
@@ -2,7 +2,7 @@ import { CIDR } from "./cidr"
 
 describe("CIDR", () => {
   it("should create an valid ipv4 CIDR object", () => {
-    const cidr = new CIDR("127.0.0.1/24")
+    const cidr = CIDR.parse("127.0.0.1/24")
     expect(cidr.getNetworkAddress().version).toBe("v4")
     expect(cidr.getNetworkType()).toBe("Loopback")
   })
